7a004d60250e438ba093b25024a6d04465c7b07b
Marcos Hernández Rodríguez <marcoshernandezhr@gmail.com>
2021-09-11 17:46:16 +0100

    Create battery.py

---
+++ battery.py
@@ -0,0 +1,10 @@
+import psutil
+
+battery = psutil.sensors_battery()
+plugged = battery.power_plugged
+percent = str(battery.percent)
+
+if plugged==False: plugged="Not Plugged In"
+else: plugged="Plugged In"
+
+print(percent+'% | '+plugged)
